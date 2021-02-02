6dd992f7850cfbf4e68cbf39624ca6dbfae3def3
Vcuber <Vcuber@users.noreply.github.com>
2021-02-02 22:11:31 +0530

    used procedural instead of OO

--- p10.php
+++ p10.php
@@ -1,6 +1,11 @@
 <html>
     <head>
         <title>Program 10</title>
+        <style>
+            table {
+                border: 1px solid black;
+            }
+        </style>
     </head>
     <body>
         <h1>Selection Sort - Student List</h1>
@@ -14,13 +19,13 @@
 
         $query = "SELECT * from student_tbl";
 
-        if($result = $connect->query($query)) {
+        if($result = mysqli_query($connect, $query)) {
 
             echo "<h3>List before sorting</h3>";
             
             echo "<table><tr><th></th><th>USN</th><th>Name</th><th>Branch</th><th>Batch</th></tr>";
 
-            while($row = $result->fetch_assoc()) {
+            while($row = mysqli_fetch_assoc($result)) {
                 echo "<tr><td>".$row["ID"]."</td>";
                 echo "<td>".$row["USN"]."</td>";
                 echo "<td>".$row["Name"]."</td>";
@@ -30,17 +35,17 @@
             echo "</table>";
         }
 
-        $result = $connect->query($query);
+        $result = mysqli_query($connect, $query);
         $rowcount = mysqli_num_rows($result);
 
         $i = 0;
         $j = 0;
 
-        while($row=$result->fetch_array()) {
+        while($row=mysqli_fetch_array($result)) {
             $minID = $row[0];
             $minUSN = $row[1];
 
-            while($urow = $result->fetch_array()) {
+            while($urow = mysqli_fetch_array($result)) {
                 if(strnatcmp($urow[1], $minUSN)<0) {
                     $minID = $urow[0];
                     $minUSN = $urow[1];
@@ -48,33 +53,33 @@
             }
 
             $query_min = "SELECT * FROM student_tbl WHERE ID=$minID";
-            $result_min = $connect->query($query_min);
-            $min_rec = $result_min->fetch_array();
+            $result_min = mysqli_query($connect, $query_min);
+            $min_rec = mysqli_fetch_array($result_min);
 
             $query_swap = "UPDATE student_tbl SET USN='$row[1]', Name='$row[2]' WHERE ID=$minID";
             "</br>";
 
-            $result_swap = $connect->query($query_swap);
+            $result_swap = mysqli_query($connect, $query_swap);
             
             $query_swap2 = "UPDATE student_tbl SET USN='$min_rec[1]', Name='$row[2]' WHERE ID=$row[0]";
             "<br /><br />";
 
-            $result_swap = $connect->query($query_swap2);
+            $result_swap = mysqli_query($connect, $query_swap2);
 
             $i++;
 
             if($i == $rowcount-1) break;
-            $result = $connect->query($query);
+            $result = mysqli_query($connect, $query);
 
             mysqli_data_seek($result, $i);
         }
 
-        if($result = $connect->query($query)) {
+        if($result = mysqli_query($connect, $query)) {
             echo "<h3>List after sorting</h3>";
 
             echo "<table><tr><th></th><th>USN</th><th>Name</th><th>Branch</th><th>Batch</th></tr>";
 
-            while($row = $result->fetch_assoc()) {
+            while($row = mysqli_fetch_assoc($result)) {
                 echo "<tr><td>".$row["ID"]."</td>";
                 echo "<td>".$row["USN"]."</td>";
                 echo "<td>".$row["Name"]."</td>";
