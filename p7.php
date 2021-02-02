1b131d547db26bd7fc7276a9858029800e7b8c1a
Vcuber <Vcuber@users.noreply.github.com>
2021-02-02 12:45:10 +0530

    Create p7.php

---
+++ p7.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Program 7</title>
+        <meta http-equiv="refresh" content="1" />
+        <style>
+            #clock {
+                color: white;
+                font-size: 90px;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                background-color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <h2>Digital Clock</h2>
+
+        <div id="clock">
+        <?php 
+            date_default_timezone_set('Asia/Kolkata');
+            echo date(" h: i : s A"); 
+        ?>
+        </div>
+    </body>
+</html>
